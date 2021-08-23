918728bde7b122fa334e38e4feef14ad4445239f
Juan Carlos Wu <jczas@sina.com>
2021-08-23 17:58:41 -0400

    Added Earth 3D model

---
+++ threejs/examples/earth/earth.js
@@ -0,0 +1,55 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.131.3";
+import { WEBGL } from "https://cdn.skypack.dev/three@0.131.3/examples/jsm/WebGL.js";
+
+if (WEBGL.isWebGLAvailable()) {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer({ antialias: false });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+
+  // sphere object that will be the earth
+  const earth = new THREE.Mesh(
+    new THREE.SphereGeometry(0.6, 32, 32),
+    new THREE.MeshPhongMaterial({
+      map: new THREE.TextureLoader().load("./earth-uv-map.jpeg"),
+      bumpMap: new THREE.TextureLoader().load("./earth-bump-map.jpeg"),
+      bumpScale: 0.3,
+    })
+  );
+
+  const clouds = new THREE.Mesh(
+    new THREE.SphereGeometry(0.63, 32, 32),
+    new THREE.MeshPhongMaterial({
+      map: new THREE.TextureLoader().load("./clouds.png"),
+      transparent: true,
+    })
+  );
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(5, 3, 5);
+
+  scene.add(ambientLight);
+  scene.add(pointLight);
+  scene.add(earth);
+  scene.add(clouds);
+
+  camera.position.z = 2;
+
+  function animate() {
+    window.requestAnimationFrame(animate);
+    earth.rotation.y -= 0.0015;
+    clouds.rotation.y -= 0.001;
+    renderer.render(scene, camera);
+  }
+  animate();
+} else {
+  window.alert("WebGL is not available.");
+}
