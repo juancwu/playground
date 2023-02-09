a87858235fc1668fdb47e8a3eec696dc836914e9
juancwu <46619361+juancwu@users.noreply.github.com>
2023-02-09 01:11:29 -0500

    add firebase functions

---
+++ notion/notion-integration/functions/index.js
@@ -0,0 +1,32 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const writeResult = await admin
+    .firestore()
+    .collection("messages")
+    .add({ original });
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    // Grab the current value of what was written to Firestore.
+    const original = snap.data().original;
+
+    // Access the parameter `{documentId}` with `context.params`
+    functions.logger.log("Uppercasing", context.params.documentId, original);
+
+    const uppercase = original.toUpperCase();
+
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to Firestore.
+    // Setting an 'uppercase' field in Firestore document returns a Promise.
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
